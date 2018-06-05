1ead170dc6d2e7647f1ed8cca4e990dba4966dac
Unknown <mtigrini@gmail.com>
2018-06-05 15:01:41 -0400

    Je suis maintenant capable de faire apparaître ma propre grille et de rediriger à la bonne ressource lorsquun utilisateur clique sur une donnée de la table.

--- src/app/event/volunteer/volunteer-index/volunteer-index.component.ts
+++ src/app/event/volunteer/volunteer-index/volunteer-index.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormioGridComponent } from 'angular-formio/grid/grid.component';
 import { FormioGrid } from 'angular-formio/grid/grid.module';
+import { FormioResource, FormioResourceService } from 'angular-formio/resource';
+import {FormioResourceConfig} from 'angular-formio/resource/resource.config';
+import { FormioService, FormioLoader, FormioAlerts } from 'angular-formio';
+import FormioUtils from 'formiojs/utils';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,12 +14,32 @@ import { FormioGrid } from 'angular-formio/grid/grid.module';
   styleUrls: ['./volunteer-index.component.scss']
 })
 export class VolunteerIndexComponent implements OnInit {
+  @ViewChild('grid2') public grid2: FormioGridComponent;
+  public grid: FormioGridComponent;
+  public router: Router;
 
-  constructor() {
-    console.log(FormioGridComponent.prototype.lastItem);
-   }
+  constructor(private service: FormioResourceService, private service2: FormioGrid) {
+    // console.log(service);
+    // console.log(grid);
+    // console.log(service);
+  }
 
   ngOnInit() {
+    const loader: FormioLoader = new FormioLoader();
+    const alerts: FormioAlerts = new FormioAlerts();
+    this.grid = new FormioGridComponent(loader, alerts);
+    this.grid.ngOnInit();
+    this.grid.src = 'https://jdggmmtbamlmyav.form.io/volunteer';
+    this.grid.loadGrid('https://jdggmmtbamlmyav.form.io/volunteer/');
+    // console.log(this.grid);
   }
+
+  afterLoad(row: any) {
+    console.log(this.router);
+    console.log(row);
+  }
+
+
+
 
 }
